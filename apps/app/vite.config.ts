a88552b188d52e471f526b0c9fac612fdd752baa
Daniil Pankov <dpancov64@gmail.com>
2026-07-24 02:10:01 +0300

    chore(app): add source import alias

--- apps/app/vite.config.ts
+++ apps/app/vite.config.ts
@@ -1,9 +1,16 @@
+import { fileURLToPath, URL } from 'node:url';
+
 import { defineConfig } from 'vite';
 import solid from 'vite-plugin-solid';
 
 export default defineConfig({
   base: './',
   plugins: [solid()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
   optimizeDeps: {
     exclude: ['@sqlite.org/sqlite-wasm'],
   },
