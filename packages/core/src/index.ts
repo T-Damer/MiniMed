84949f8e374663cdb6a003a57bd9f5f6c491f713
Daniil Pankov <dpancov64@gmail.com>
2026-07-30 18:10:11 +0300

    feat(core): export the L-Note adapter

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,5 +3,6 @@ export * from './content-module-installer';
 export * from './create-medical-core';
 export * from './document-siblings';
 export * from './in-memory';
+export * from './l-note-adapter';
 export * from './mappers';
 export * from './search-context';
