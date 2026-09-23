9ca415e4e198664a6ed28a7af82213920074b79b
Daniil Pankov <dpancov64@gmail.com>
2026-09-23 20:45:57 +0300

    refactor(search): expose existing definition question parser to storage adapters

--- packages/search-lexical/src/index.ts
+++ packages/search-lexical/src/index.ts
@@ -3,6 +3,7 @@ export type { ClinicalQueryPlan, LexicalQueryBranchPlan } from './analysis';
 export { buildLookupQueryPlan } from './analysis';
 export { analyzeClinicalQuery } from './clinical-query';
 export * from './definition-description';
+export * from './definition-question';
 export * from './html-markup';
 export * from './intent';
 export * from './normalize';
